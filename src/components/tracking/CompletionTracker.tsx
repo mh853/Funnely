4dc735842ef89e2f76489d20c5d698de9f06c456
최문호 <mh.c@MacBook-Pro.local>
2026-01-09 16:50:15 +0900

    feat: TikTok 픽셀 완료 페이지 추적 추가
    - CompletionTracker에 tiktok_pixel_id 필드 추가
    - 완료 페이지에서 TikTok 픽셀 스크립트 로드
    - ttq.page() 및 ttq.track('CompleteRegistration') 이벤트 전송
    - 공개 랜딩페이지는 이미 TikTok 픽셀 구현 완료됨
    
    TikTok Pixel ID: D5GB10JC77U666PPHTKG
    이제 완료 페이지에서 TikTok 픽셀 이벤트가 전송됩니다.

--- src/components/tracking/CompletionTracker.tsx
+++ src/components/tracking/CompletionTracker.tsx
@@ -7,6 +7,7 @@ interface CompletionTrackerProps {
     facebook_pixel_id?: string
     google_analytics_id?: string
     kakao_pixel_id?: string
+    tiktok_pixel_id?: string
     is_active?: boolean
   }
 }
@@ -88,6 +89,24 @@ export default function CompletionTracker({ trackingPixels }: CompletionTrackerP
               i.parentNode.insertBefore(o,i)}(window,document,"script","kakaoPixel");
               kakaoPixel('${trackingPixels.kakao_pixel_id}').pageView();
               kakaoPixel('${trackingPixels.kakao_pixel_id}').completeRegistration();
+            `,
+          }}
+        />
+      )}
+
+      {/* TikTok Pixel */}
+      {trackingPixels?.is_active && trackingPixels?.tiktok_pixel_id && (
+        <Script
+          id="tiktok-pixel-completion"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+                ttq.load('${trackingPixels.tiktok_pixel_id}');
+                ttq.page();
+                ttq.track('CompleteRegistration');
+              }(window, document, 'ttq');
             `,
           }}
         />
